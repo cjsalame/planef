e2d6432f5dc52198c659393e975949b2ea134934
Charly <cjsalame@uc.cl>
2017-06-12 02:11:28 -0400

    users avatars working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :avatar_file
+  after_save :save_avatar, if: :avatar_file
   devise                  :database_authenticatable, :registerable,
                           :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :schools
@@ -12,4 +14,17 @@ class User < ApplicationRecord
                                 allow_destroy: true,
                                 reject_if: proc { |att| att['id'].blank? }
   validates_associated :subjects_teachers
+
+  def save_avatar
+    filename = avatar_file.original_filename
+    folder = "app/assets/images/users/#{id}/avatar"
+    puts '-----------------------------------------------------------------------------------------------------'
+    FileUtils.mkdir_p folder
+    f = File.open File.join(folder, filename), 'wb'
+    f.write avatar_file.read
+    f.close
+
+    self.avatar_file = nil
+    update avatar: filename
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516210252) do
+ActiveRecord::Schema.define(version: 20170612014204) do
 
   create_table "expected_learnings", force: :cascade do |t|
     t.string "code"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20170516210252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
