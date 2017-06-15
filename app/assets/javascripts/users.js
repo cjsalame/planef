4e2c4cececbbf4fb22a18b72c20c692a264469ab
fdjlss2 <jlschellman@uc.cl>
2017-06-15 19:32:11 -0400

    NOW they can..

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,33 +1,77 @@
 var school_validation = $(function(){
 
-	$("#school-form-submit").on("click", function(e){
+	var school_code = ""
+	
+	// opción 1)
+	// $("#school-form-submit").on("click", function(e){
+	// 	e.preventDefault();
 
-		// opción 1)
-		// var school_code = $("select#schoolid").find(":selected").data("schoolCode");
-		// var inputed_code = $("[name='schoolCode']").val();
-		// if( !(school_code === inputed_code) ){
-		// 	e.preventDefault();
-		// 	alert('Error: código ingresado no corresponde al código del colegio');
-		// }
+	// 	var school_id = $("select#schoolid").find(":selected").val();
+	// 	var inputed_code = $("[name='schoolCode']").val();
 
+	// 	$.ajax({
+	// 		type: "GET",
+	// 		url: "/schools/" + school_id + ".json",			
+	// 		dataType: "json",
+	// 		success: function(data, ts, jq) {
+	// 			school_code = data.code;
+	// 			console.log("School Code: " + school_code);
+	// 			console.log("Inputed Code: " + inputed_code);
+	// 		},
+			
+	// 		// error: function(jq, st, er) {
+	// 		// 	e.preventDefault();
+	// 		// 	alert("Error: No se pudo ingresar colegio");
+	// 		// 	console.log("Error!", jq, st, er);
+	// 		// }
 
+	// 	})
+	// 	.done(function(data){
+	// 		console.log("Data: " + data.code)
+	// 		if( data.code === inputed_code ) {
+	// 			// Registra el colegio en su cuenta..
+	// 			$(this).unbind("click");
+	// 			console.log("Son iguales");
+	// 		}
+	// 		else {
+	// 			console.log("No son iguales..");
+	// 			e.preventDefault();
+	// 			alert('Error: Código ingresado no corresponde al código del colegio');
+	// 		}
 
-		// opción 2)
-		e.preventDefault();
+	// 	});
 
+	// });
+
+	// opción 2)
+	$("select#schoolid option").on("click", function(e){
 		var school_id = $("select#schoolid").find(":selected").val();
-		
+
 		$.ajax({
 			type: "GET",
 			url: "/schools/" + school_id + ".json",
 			dataType: "json",
 			success: function(data, ts, jq) {
-				console.log("School Data Retrieved!: " + data.code);
-				var school_code = data.code;
+				school_code = data.code;
+				console.log("School Code: " + school_code);
+			},
+			error: function(jq, st, er) {
+				alert("Error seleccionando colegio");
+				console.log("Error!", jq, st, er);
 			}
 		});
 
+
+	});
+
+	$("#school-form-submit").on("click", function(e){
 		var inputed_code = $("[name='schoolCode']").val();
+		if( school_code === inputed_code ){
+			// Registra el colegio en su cuenta..
+		} else {
+			e.preventDefault();
+			alert('Error: Código ingresado no corresponde al código del colegio');
+		}
 
 	});
 
