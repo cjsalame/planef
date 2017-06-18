eea9653b9318b83dbed059c89060b24b0761588b
Emily White <froggyeels@hotmail.com>
2017-06-18 17:07:04 -0400

    fixed buttons and schools css

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,14 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema
-# definition.
+# incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more
-# migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control
-# system.
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170614200603) do
 
@@ -50,6 +47,8 @@ ActiveRecord::Schema.define(version: 20170614200603) do
     t.integer "planification_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "expected_learning_id"
+    t.index ["expected_learning_id"], name: "index_lectures_on_expected_learning_id"
     t.index ["planification_id"], name: "index_lectures_on_planification_id"
   end
 
@@ -122,4 +121,5 @@ ActiveRecord::Schema.define(version: 20170614200603) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
