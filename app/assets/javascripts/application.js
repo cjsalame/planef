8ec3cb36acecf0c00b4164e7a6bbd52b672e69ea
fdjlss2 <jlschellman@uc.cl>
2017-06-23 17:04:10 -0400

    jorge's last edit for demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,22 +30,120 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-$(function() {
-  return $(document).on("click", "#add-lecture-link", function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: '/planifications/add_lecture.html',
 
-      success: function(data) {
-        var el_to_add;
-        el_to_add = $(data).html();
-    		$('#lecture-form').append(el_to_add);
-      },
+function state_shift(button_el, boolean_attr, state_msg, counterpart) {
 
-      error: function(data) {
-        return alert("Sorry, There Was An Error!");
+  return function() {
+    $(button_el).on("click", function(e) {
+      console.log("CLICK!");
+      $that = $(this);
+      planification_id = $that.data("planificationId");
+
+
+      if(counterpart === 'profesor') {
+        $(".plan-state-change-utp").on("click", function(e) {
+          if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
+            $.ajax({
+              type: "PUT",
+              url: "/planifications/" + planification_id + ".json",
+              data: { "planification[state]": boolean_attr, 
+                      "planification[utpcomment]" : $(".modal-jorge textarea").val() },
+              dataType: "JSON",
+              success: function(data, ts, jq){
+                console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
+                // Cambia el mensaje en columna "Estado"
+                $(".plan_state").text( state_msg );
+                // Hace al botón unclickable luego de mandar correo
+                $that.attr("disabled", true);
+                
+                console.log("Comentario: " + $(".modal-jorge textarea").val() );
+                alert("¡Correo enviado!");
+              }
+            });
+          }
+        });
       }
+      else {
+        if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
+          $.ajax({
+            type: "PUT",
+            url: "/planifications/" + planification_id + ".json",
+            data: { "planification[state]": boolean_attr },
+            dataType: "JSON",
+            success: function(data, ts, jq){
+              console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
+              // Cambia el mensaje en columna "Estado"
+              $(".plan_state").text( state_msg );
+              // Hace al botón unclickable
+              $that.attr("disabled", true);
 
+              alert("¡Correo enviado!");
+            }
+          });
+        } 
+      }
     });
+  }
+}
+
+
+
+
+var modal_jorge = function() {
+
+  // var triggers = $(".modal-jorgeInput").overlay({
+  //   mask: {
+  //     color: '#ebecff',
+  //     loadSpeed: 200,
+  //     opacity: 0.9
+  //   },
+
+  //   closeOnClick: false
+  // });
+
+  plan_id = $(".modalInput.plan_id").data("planificationId");
+
+  var $mb = $('.modal-jorge.' + plan_id);
+
+  $(".modalInput." + plan_id + ".plan_id").on("click", function(e) {
+    console.log("CLICK!");
+    $mb.css('display', 'block');
+    $mb.animate({ opacity: 0 }, 0);
+    $mb.animate({ opacity: 1, top: "10px" }, 'slow');
   });
-}) ();
+
+  // Cerramos el cuadro modal-jorge al hacer click en cualquier parte
+  $mb.on('click', function(ev) {
+    $mb.animate({ opacity: 1, top: "0px" }, 'slow');
+    $mb.animate({ opacity: 0 }, 0);
+    $mb.css('display', 'none');
+  });
+  // Evitamos bubbling en los elementos que están dentro del mismo cuadro (el <div> dentro de <div class="modal-jorge-box"> )
+  $('.modal-jorge div').on('click', function(ev) {
+    ev.stopPropagation();
+  });
+
+  // Se cierra el cuadro modal-jorge al hacer click en la [X]
+  $('.modal-jorge .X').on('click', function(ev) {
+    $mb.animate({ opacity: 1, top: "0px" }, 'slow');
+    $mb.animate({ opacity: 0 }, 0);
+    $mb.css('display', 'none');
+  });
+
+
+  // $(".close").on("click", function (e) {
+  //   triggers.eq(1).overlay().close();
+
+  //   return e.preventDefault();
+  // });
+
+
+}
+
+
+var prof_shift = state_shift(".plan-state-change-prof", false, "Revisión por UTP", 'Jefe UTP');
+var utp_shift = state_shift(".modalInput", true, "Edición por Profesor", 'profesor' );
+
+$(document).on('turbolinks:load', prof_shift);
+$(document).on('turbolinks:load', utp_shift);
+$(document).on('turbolinks:load', modal_jorge);
--- app/assets/javascripts/planification.js
+++ app/assets/javascripts/planification.js
@@ -33,116 +33,117 @@ var lecture_addition = function() {
 //   });
 // }
 
-function state_shift(button_el, boolean_attr, state_msg, counterpart) {
+// function state_shift(button_el, boolean_attr, state_msg, counterpart) {
 
-  return function() {
-    $(button_el).on("click", function(e) {
-      console.log("CLICK!");
-      $that = $(this);
-      planification_id = $that.data("planificationId");
-
-
-      if(counterpart === 'profesor') {
-        $("#plan-state-change-utp").on("click", function(e) {
-          if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
-            $.ajax({
-              type: "PUT",
-              url: "/planifications/" + planification_id + ".json",
-              data: { "planification[state]": boolean_attr,
-                      "planification[utpcomment]" : $("#plan_comment").val() },
-              dataType: "JSON",
-              success: function(data, ts, jq){
-                console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
-                // Cambia el mensaje en columna "Estado"
-                $("#plan-state-show").text( state_msg );
-                // Hace al botón unclickable luego de mandar correo
-                $that.attr("disabled", true);
-
-                console.log("Comentario: " + $("#plan_comment").val() );
-                alert("¡Correo enviado!");
-              }
-            });
-          }
-        });
-      }
-      else {
-        if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
-          $.ajax({
-            type: "PUT",
-            url: "/planifications/" + planification_id + ".json",
-            data: { "planification[state]": boolean_attr },
-            dataType: "JSON",
-            success: function(data, ts, jq){
-              console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
-              // Cambia el mensaje en columna "Estado"
-              $("#plan-state-show").text( state_msg );
-              // Hace al botón unclickable
-              $that.attr("disabled", true);
-
-              alert("¡Correo enviado!");
-            }
-          });
-        }
-      }
-    });
-  }
-}
+//   return function() {
+//     $(button_el).on("click", function(e) {
+//       console.log("CLICK!");
+//       $that = $(this);
+//       planification_id = $that.data("planificationId");
 
 
-var modal-jorge = function() {
+//       if(counterpart === 'profesor') {
+//         $("#plan-state-change-utp").on("click", function(e) {
+//           if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
+//             $.ajax({
+//               type: "PUT",
+//               url: "/planifications/" + planification_id + ".json",
+//               data: { "planification[state]": boolean_attr,
+//                       "planification[utpcomment]" : $("#plan_comment").val() },
+//               dataType: "JSON",
+//               success: function(data, ts, jq){
+//                 console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
+//                 // Cambia el mensaje en columna "Estado"
+//                 $("#plan-state-show").text( state_msg );
+//                 // Hace al botón unclickable luego de mandar correo
+//                 $that.attr("disabled", true);
 
-  // var triggers = $(".modal-jorgeInput").overlay({
-  //   mask: {
-  //     color: '#ebecff',
-  //     loadSpeed: 200,
-  //     opacity: 0.9
-  //   },
+//                 console.log("Comentario: " + $("#plan_comment").val() );
+//                 alert("¡Correo enviado!");
+//               }
+//             });
+//           }
+//         });
+//       }
+//       else {
+//         if ( confirm('¿Enviar e-mail a ' + counterpart + '?') ) {
+//           $.ajax({
+//             type: "PUT",
+//             url: "/planifications/" + planification_id + ".json",
+//             data: { "planification[state]": boolean_attr },
+//             dataType: "JSON",
+//             success: function(data, ts, jq){
+//               console.log("Planification " + planification_id + " State Changed to " + boolean_attr);
+//               // Cambia el mensaje en columna "Estado"
+//               $("#plan-state-show").text( state_msg );
+//               // Hace al botón unclickable
+//               $that.attr("disabled", true);
 
-  //   closeOnClick: false
-  // });
-
-  var $mb = $('.modal-jorge');
-
-  $(".modal-jorgeInput").on("click", function(e) {
-    console.log("CLICK!");
-    $mb.css('display', 'block');
-    $mb.animate({ opacity: 0 }, 0);
-    $mb.animate({ opacity: 1, top: "10px" }, 'slow');
-  });
-
-  // Cerramos el cuadro modal-jorge al hacer click en cualquier parte
-  $mb.on('click', function(ev) {
-    $mb.animate({ opacity: 1, top: "0px" }, 'slow');
-    $mb.animate({ opacity: 0 }, 0);
-    $mb.css('display', 'none');
-  });
-  // Evitamos bubbling en los elementos que están dentro del mismo cuadro (el <div> dentro de <div class="modal-jorge-box"> )
-  $('.modal-jorge div').on('click', function(ev) {
-    ev.stopPropagation();
-  });
-
-  // Se cierra el cuadro modal-jorge al hacer click en la [X]
-  $('.modal-jorge .X').on('click', function(ev) {
-    $mb.animate({ opacity: 1, top: "0px" }, 'slow');
-    $mb.animate({ opacity: 0 }, 0);
-    $mb.css('display', 'none');
-  });
-
-
-  // $(".close").on("click", function (e) {
-  //   triggers.eq(1).overlay().close();
-
-  //   return e.preventDefault();
-  // });
-
-
-}
+//               alert("¡Correo enviado!");
+//             }
+//           });
+//         }
+//       }
+//     });
+//   }
+// }
 
 
-var prof_shift = state_shift("#plan-state-change-prof", false, "Revisión por UTP", 'Jefe UTP');
-var utp_shift = state_shift(".modal-jorgeInput.plan_id", true, "Edición por Profesor", 'profesor' );
+// // var modal-jorge = function() {
+
+// //   // var triggers = $(".modal-jorgeInput").overlay({
+// //   //   mask: {
+// //   //     color: '#ebecff',
+// //   //     loadSpeed: 200,
+// //   //     opacity: 0.9
+// //   //   },
+
+// //   //   closeOnClick: false
+// //   // });
+
+// //   var $mb = $('.modal-jorge');
+
+// //   $(".modal-jorgeInput").on("click", function(e) {
+// //     console.log("CLICK!");
+// //     $mb.css('display', 'block');
+// //     $mb.animate({ opacity: 0 }, 0);
+// //     $mb.animate({ opacity: 1, top: "10px" }, 'slow');
+// //   });
+
+// //   // Cerramos el cuadro modal-jorge al hacer click en cualquier parte
+// //   $mb.on('click', function(ev) {
+// //     $mb.animate({ opacity: 1, top: "0px" }, 'slow');
+// //     $mb.animate({ opacity: 0 }, 0);
+// //     $mb.css('display', 'none');
+// //   });
+// //   // Evitamos bubbling en los elementos que están dentro del mismo cuadro (el <div> dentro de <div class="modal-jorge-box"> )
+// //   $('.modal-jorge div').on('click', function(ev) {
+// //     ev.stopPropagation();
+// //   });
+
+// //   // Se cierra el cuadro modal-jorge al hacer click en la [X]
+// //   $('.modal-jorge .X').on('click', function(ev) {
+// //     $mb.animate({ opacity: 1, top: "0px" }, 'slow');
+// //     $mb.animate({ opacity: 0 }, 0);
+// //     $mb.css('display', 'none');
+// //   });
+
+
+// //   // $(".close").on("click", function (e) {
+// //   //   triggers.eq(1).overlay().close();
+
+// //   //   return e.preventDefault();
+// //   // });
+
+
+// // }
+
+
+// var prof_shift = state_shift("#plan-state-change-prof", false, "Revisión por UTP", 'Jefe UTP');
+// var utp_shift = state_shift(".modal-jorgeInput.plan_id", true, "Edición por Profesor", 'profesor' );
 
 $(document).on('turbolinks:load', lecture_addition);
-$(document).on('turbolinks:load', prof_shift);
-$(document).on('turbolinks:load', utp_shift);
-$(document).on('turbolinks:load', modal-jorge);
+// $(document).on('turbolinks:load', prof_shift);
+// $(document).on('turbolinks:load', utp_shift);
+// $(document).on('turbolinks:load', modal-jorge);
+
